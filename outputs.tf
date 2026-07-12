10ee7b064dc9c84845404627781edbd9c95199e7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "virtual_hub_connections" {
-  description = "All virtual_hub_connection resources"
-  value       = azurerm_virtual_hub_connection.virtual_hub_connections
-}
 output "virtual_hub_connections_internet_security_enabled" {
-  description = "List of internet_security_enabled values across all virtual_hub_connections"
-  value       = [for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : v.internet_security_enabled]
+  description = "Map of internet_security_enabled values across all virtual_hub_connections, keyed the same as var.virtual_hub_connections"
+  value       = { for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : k => v.internet_security_enabled }
 }
 output "virtual_hub_connections_name" {
-  description = "List of name values across all virtual_hub_connections"
-  value       = [for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : v.name]
+  description = "Map of name values across all virtual_hub_connections, keyed the same as var.virtual_hub_connections"
+  value       = { for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : k => v.name }
 }
 output "virtual_hub_connections_remote_virtual_network_id" {
-  description = "List of remote_virtual_network_id values across all virtual_hub_connections"
-  value       = [for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : v.remote_virtual_network_id]
+  description = "Map of remote_virtual_network_id values across all virtual_hub_connections, keyed the same as var.virtual_hub_connections"
+  value       = { for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : k => v.remote_virtual_network_id }
 }
 output "virtual_hub_connections_routing" {
-  description = "List of routing values across all virtual_hub_connections"
-  value       = [for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : v.routing]
+  description = "Map of routing values across all virtual_hub_connections, keyed the same as var.virtual_hub_connections"
+  value       = { for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : k => v.routing }
 }
 output "virtual_hub_connections_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_connections"
-  value       = [for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_connections, keyed the same as var.virtual_hub_connections"
+  value       = { for k, v in azurerm_virtual_hub_connection.virtual_hub_connections : k => v.virtual_hub_id }
 }
 
